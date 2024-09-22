c85611d763088b17c1d543e8727b4de96c8dc5f0
takahiro-okada <okahanako09875@gmail.com>
2024-09-23 05:11:39 +0900

    ✏️: Update allusers api

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,7 +6,23 @@ const prisma = new PrismaClient();
 export const GET = async () => {
   try {
     await prisma.$connect();
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      include: {
+        currentAddress: {
+          include: {
+            prefecture: true,
+            country: true,
+          },
+        },
+        deploymentLocation: {
+          include: {
+            country: true,
+          },
+        },
+        cohort: true,
+      },
+    });
+
     return NextResponse.json(users, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: 'Server error', err }, { status: 500 });
