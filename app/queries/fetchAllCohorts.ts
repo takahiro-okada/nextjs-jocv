a263a62633ef1410b562664082b2ca565d0bee9a
takahiro-okada <okahanako09875@gmail.com>
2024-10-05 07:48:49 +0900

    ✏️: Change env name

--- app/queries/fetchAllCohorts.ts
+++ app/queries/fetchAllCohorts.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
 
 async function fetchAllCohorts() {
   const res = await fetch(`${API_URL}/api/cohorts`, {
--- app/queries/fetchUser.ts
+++ app/queries/fetchUser.ts
@@ -1,6 +1,6 @@
 import { UserType } from '@/app/type';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
 
 async function fetchUser(userId: number): Promise<UserType | null> {
   const res = await fetch(`${API_URL}/api/users/${userId}`, {
